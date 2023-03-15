012e60f4f0ac55ea57686b3281524ea62c2f2fb7
suhas-sunder <77464593+suhas-sunder@users.noreply.github.com>
2023-03-14 22:30:35 -0400

    User registration complete
    User registration form implemented with basic authentication and proper styling.

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,19 +1,47 @@
-import React from 'react';
+import React, { useState } from "react";
+import Card from "../UI/Card";
+import styles from '../Users/AddUser.module.css'
+import Button from "../UI/Button";
 
 function AddUser() {
-    const addUserHandler = (event) => {
-        event.preventDefault();
+    const [enteredUserName, setEnteredUserName] = useState('');
+    const [enteredAge, setEnteredAge] = useState('');
+
+  const addUserHandler = (event) => {
+    event.preventDefault();
+    
+    if(enteredUserName.trim().length === 0 || enteredAge.trim().length === 0) {
+        return;
     }
 
+    if(+enteredAge < 1) {
+        return;
+    }
+
+    console.log(enteredUserName, enteredAge);
+    setEnteredAge('');
+    setEnteredUserName('');
+  };
+
+  const userNameChangeHandler = (event) => {
+    setEnteredUserName(event.target.value);
+  }
+
+  const ageChangeHandler = (event) => {
+    setEnteredAge(event.target.value);
+  }
+
   return (
-    <form onSubmit={addUserHandler}>
+    <Card className={styles.input}>
+      <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text"></input>
+        <input id="username" type="text" value={enteredUserName} onChange={userNameChangeHandler}></input>
         <label htmlFor="age">Age (Years)</label>
-        <input id="age" type="number"></input>
-        <button type="submit">Add User</button>
-    </form>
-  )
+        <input id="age" type="number" value={enteredAge} onChange={ageChangeHandler}></input>
+        <Button type="submit">Add User</Button>
+      </form>
+    </Card>
+  );
 }
 
 export default AddUser;
